38623b1ea0c9c242153b662c78a4cbe4ee3d4047
Leigh-Anne Mathieson <leigh-anne.mathieson@guardian.co.uk>
2019-04-16 15:26:53 +0100

    Add linear mean squared error impl

--- src/main/scala/util/univariate/LinearErrorCalculator.scala
+++ src/main/scala/util/univariate/LinearErrorCalculator.scala
@@ -4,5 +4,14 @@ import model.univariate.SimplePoint
 
 object LinearErrorCalculator {
 
-  def linearMeanSquaredError(data: List[SimplePoint], theta0: Double, theta1: Double): Double = ???
+  def linearMeanSquaredError(data: List[SimplePoint], theta0: Double, theta1: Double): Double = {
+    def h(x: Double) = theta1 * x + theta0
+    def errorForAPoint(point: SimplePoint) = (h(point.x) - point.y) * (h(point.x) - point.y)
+
+    val summedErrors = data.foldLeft(0.0)((sum, point) => sum + errorForAPoint(point))
+
+    val numberOfDataPointsDivisor: Double = 0.5 * (1/data.length.toDouble)
+    summedErrors * numberOfDataPointsDivisor
+  }
+
 }
